9f538375420bbe42000c2018414caec7dca0e8dc
TakWolf <takwolf@foxmail.com>
2023-05-01 14:39:35 +0800

    代码整理

--- font_service.py
+++ font_service.py
@@ -15,18 +15,17 @@ color_border = 2
 
 def create_font_sheet(
         font_size,
-        output_name,
-        output_dir,
+        outputs_name,
+        outputs_dir,
         font_file_path,
-        rasterize_offset_x=0,
-        rasterize_offset_y=0,
-        rasterize_adjust_width=0,
-        rasterize_adjust_height=0,
+        glyph_offset_x=0,
+        glyph_offset_y=0,
+        glyph_adjust_width=0,
+        glyph_adjust_height=0,
 ):
     # 加载字体文件
     font = TTFont(font_file_path)
-    units_per_em = font['head'].unitsPerEm
-    px_units = units_per_em / font_size
+    px_units = font['head'].unitsPerEm / font_size
     hhea = font['hhea']
     metrics = font['hmtx'].metrics
     cmap = font.getBestCmap()
@@ -35,7 +34,7 @@ def create_font_sheet(
 
     # 计算行高
     line_height = math.ceil((hhea.ascent - hhea.descent) / px_units)
-    line_height += rasterize_adjust_height
+    line_height += glyph_adjust_height
 
     # 图集对象，初始化左边界
     sheet_data = [[color_border] for _ in range(line_height)]
@@ -50,13 +49,13 @@ def create_font_sheet(
             continue
 
         advance_width = math.ceil(metrics[glyph_name][0] / px_units)
-        advance_width += rasterize_adjust_width
+        advance_width += glyph_adjust_width
         if advance_width <= 0:
             continue
 
         # 栅格化
         glyph_image = Image.new('RGBA', (advance_width, line_height), (0, 0, 0, 0))
-        ImageDraw.Draw(glyph_image).text((rasterize_offset_x, rasterize_offset_y), c, fill=(0, 0, 0), font=image_font)
+        ImageDraw.Draw(glyph_image).text((glyph_offset_x, glyph_offset_y), c, fill=(0, 0, 0), font=image_font)
         logger.info(f'rasterize char: {code_point} - {c}')
 
         # 二值化字形，合并到图集
@@ -77,56 +76,56 @@ def create_font_sheet(
     sheet_data.append([color_border for _ in range(len(sheet_data[0]))])
 
     # 创建 palette 输出文件夹
-    output_palette_dir = os.path.join(output_dir, 'palette')
-    if not os.path.exists(output_palette_dir):
-        os.makedirs(output_palette_dir)
+    outputs_palette_dir = os.path.join(outputs_dir, 'palette')
+    if not os.path.exists(outputs_palette_dir):
+        os.makedirs(outputs_palette_dir)
 
     # 写入 palette .png 图集
-    output_palette_png_file_path = os.path.join(output_palette_dir, f'{output_name}.png')
+    palette_png_file_path = os.path.join(outputs_palette_dir, f'{outputs_name}.png')
     palette = [(255, 255, 255), (0, 0, 0), (255, 0, 255)]
     writer = png.Writer(len(sheet_data[0]), len(sheet_data), palette=palette)
-    with open(output_palette_png_file_path, 'wb') as file:
+    with open(palette_png_file_path, 'wb') as file:
         writer.write(file, sheet_data)
 
     # 写入 palette .dat 字母表
-    output_palette_dat_file_path = os.path.join(output_palette_dir, f'{output_name}.png.dat')
-    with open(output_palette_dat_file_path, 'w', encoding='utf-8') as file:
+    palette_dat_file_path = os.path.join(outputs_palette_dir, f'{outputs_name}.png.dat')
+    with open(palette_dat_file_path, 'w', encoding='utf-8') as file:
         file.write(''.join(alphabet))
-    logger.info(f'make {output_palette_dat_file_path}')
+    logger.info(f'make {palette_dat_file_path}')
 
     # 创建 rgba 输出文件夹
-    output_rgba_dir = os.path.join(output_dir, 'rgba')
-    if not os.path.exists(output_rgba_dir):
-        os.makedirs(output_rgba_dir)
+    outputs_rgba_dir = os.path.join(outputs_dir, 'rgba')
+    if not os.path.exists(outputs_rgba_dir):
+        os.makedirs(outputs_rgba_dir)
 
     # 写入 rgba .png 图集
-    output_rgba_png_file_path = os.path.join(output_rgba_dir, f'{output_name}.png')
-    output_rgba_bitmap = []
+    rgba_png_file_path = os.path.join(outputs_rgba_dir, f'{outputs_name}.png')
+    rgba_bitmap = []
     for sheet_data_row in sheet_data:
-        output_rgba_bitmap_row = []
+        rgba_bitmap_row = []
         for color in sheet_data_row:
             if color == color_transparent:
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
             elif color == color_solid:
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(255)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(255)
             else:
-                output_rgba_bitmap_row.append(255)
-                output_rgba_bitmap_row.append(0)
-                output_rgba_bitmap_row.append(255)
-                output_rgba_bitmap_row.append(255)
-        output_rgba_bitmap.append(output_rgba_bitmap_row)
-    image = png.from_array(output_rgba_bitmap, 'RGBA')
-    image.save(output_rgba_png_file_path)
-    logger.info(f'make {output_rgba_png_file_path}')
+                rgba_bitmap_row.append(255)
+                rgba_bitmap_row.append(0)
+                rgba_bitmap_row.append(255)
+                rgba_bitmap_row.append(255)
+        rgba_bitmap.append(rgba_bitmap_row)
+    image = png.from_array(rgba_bitmap, 'RGBA')
+    image.save(rgba_png_file_path)
+    logger.info(f'make {rgba_png_file_path}')
 
     # 写入 rgba .dat 字母表
-    output_rgba_dat_file_path = os.path.join(output_rgba_dir, f'{output_name}.png.dat')
-    with open(output_rgba_dat_file_path, 'w', encoding='utf-8') as file:
+    rgba_dat_file_path = os.path.join(outputs_rgba_dir, f'{outputs_name}.png.dat')
+    with open(rgba_dat_file_path, 'w', encoding='utf-8') as file:
         file.write(''.join(alphabet))
-    logger.info(f'make {output_rgba_dat_file_path}')
+    logger.info(f'make {rgba_dat_file_path}')
--- main.py
+++ main.py
@@ -18,41 +18,41 @@ def main():
 
     font_service.create_font_sheet(
         font_size=8,
-        output_name='quan',
-        output_dir=build_dir,
+        outputs_name='quan',
+        outputs_dir=build_dir,
         font_file_path=os.path.join(fonts_dir, 'quan', 'quan.ttf'),
-        rasterize_adjust_width=-1,
-        rasterize_adjust_height=-1,
+        glyph_adjust_width=-1,
+        glyph_adjust_height=-1,
     )
     font_service.create_font_sheet(
         font_size=12,
-        output_name='fusion-pixel-monospaced',
-        output_dir=build_dir,
+        outputs_name='fusion-pixel-monospaced',
+        outputs_dir=build_dir,
         font_file_path=os.path.join(fonts_dir, 'fusion-pixel-monospaced', 'fusion-pixel-monospaced.otf'),
-        rasterize_offset_y=-1,
-        rasterize_adjust_width=-1,
-        rasterize_adjust_height=-1,
+        glyph_offset_y=-1,
+        glyph_adjust_width=-1,
+        glyph_adjust_height=-1,
     )
     font_service.create_font_sheet(
         font_size=12,
-        output_name='fusion-pixel-proportional',
-        output_dir=build_dir,
+        outputs_name='fusion-pixel-proportional',
+        outputs_dir=build_dir,
         font_file_path=os.path.join(fonts_dir, 'fusion-pixel-proportional', 'fusion-pixel-proportional.otf'),
-        rasterize_offset_y=-1,
-        rasterize_adjust_width=-1,
-        rasterize_adjust_height=-1,
+        glyph_offset_y=-1,
+        glyph_adjust_width=-1,
+        glyph_adjust_height=-1,
     )
     font_service.create_font_sheet(
         font_size=16,
-        output_name='unifont',
-        output_dir=build_dir,
+        outputs_name='unifont',
+        outputs_dir=build_dir,
         font_file_path=os.path.join(fonts_dir, 'unifont', 'unifont-15.0.01.ttf'),
-        rasterize_adjust_width=-1,
+        glyph_adjust_width=-1,
     )
     font_service.create_font_sheet(
         font_size=24,
-        output_name='roboto',
-        output_dir=build_dir,
+        outputs_name='roboto',
+        outputs_dir=build_dir,
         font_file_path=os.path.join(fonts_dir, 'roboto', 'Roboto-Regular.ttf'),
     )
 
